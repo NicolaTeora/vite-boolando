6e0437c9542b2f78f016d5bb3a881ca22d7ec8c8
NicolaTeora <nikxodia@gmail.com>
2024-02-08 17:32:22 +0100

    feat : header-navbar

--- src/components/appHeader.vue
+++ src/components/appHeader.vue
@@ -16,6 +16,14 @@ export default{
                 <li><a href="#">Donna</a></li>
                 <li><a href="#">Bambino</a></li>
             </ul>
+
+            <h3>boolean</h3>
+
+            <ul>
+                <li><a href="#"><i class="fa-solid fa-user"></i></a></li>
+                <li><a href="#"><i class="fa-regular fa-heart"></i></a></li>
+                <li><a href="#"><i class="fa-solid fa-bag-shopping"></i></a></li>
+            </ul>
         </nav>
     </header>
 </template>
@@ -24,15 +32,20 @@ export default{
     @use '../styles/general.scss' as *;
 
     nav{
-        width: fit-content;
+        display: flex;
+        justify-content: space-between;
         padding: 5px;
+        box-shadow: 2px 1px 2px black;
         ul{
             display: flex;
-            gap: 5px;
+            
             li{
+                margin: 5px;
                 list-style: none;
                 a{
                     text-decoration: none;
+                    color: white;
+                    font-weight: bold;
                 }
             }
         }
