5c2f973309b57a1e9fb6ef88e8f04fad2d072cdf
NicolaTeora <nikxodia@gmail.com>
2024-02-08 16:35:54 +0100

    feat : develop  scaffolding: -components, -styles

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,20 @@
 <script>
+import appHeader from './components/appHeader.vue'
+import appMain from './components/appMain.vue'
+import appFooter from './components/appFooter.vue'
+
+export default{
+  data(){
+    return{
+      title:'Boolando'
+    }
+  }
+}
 
 </script>
 
 <template>
-
+  <h1>{{ title }}</h1>
 </template>
 
 <style lang="scss">
