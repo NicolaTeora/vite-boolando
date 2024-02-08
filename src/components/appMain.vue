2541ae2c6397964659b8468b3a8f288017a11d4f
NicolaTeora <nikxodia@gmail.com>
2024-02-08 18:52:05 +0100

    feat : dev methods

--- src/components/appMain.vue
+++ src/components/appMain.vue
@@ -2,7 +2,46 @@
 export default{
     data(){
         return{
-            title:'main'
+            logo: '',
+            images: [
+                {
+                    image: '1.webp',
+                    imageHover: '1b.webp'
+                },
+                {
+                    image: '2.webp',
+                    imageHover: '2b.webp'
+                },
+                {
+                    image: '3.webp',
+                    imageHover: '3b.webp'
+                },
+                {
+                    image: '4.webp',
+                    imageHover: '4b.webp'
+                },
+                {
+                    image: '5.webp',
+                    imageHover: '5b.webp'
+                },
+                {
+                    image: '6.webp',
+                    imageHover: '6b.webp'
+                },
+            ]
+        }
+    },
+
+    methods: {
+        FrontImage(imgName){
+            return new URL('../assets/img/' + imgName, import.meta.url).href;
+        },
+        HoverImage(imgName){
+            return new URL('../assets/img/' + imgName, import.meta.url).href;
+        },
+
+        LookNextImg(){
+            console.log('mouse passato')
         }
     }
 }
@@ -10,10 +49,29 @@ export default{
 
 <template>
     <main>
-        
+        <div v-for="image of images">
+            <img class="frist-img" :src="FrontImage(image.image)" @mouseover="LookNextImg()" alt="" >
+            <!-- <img class="hover-img" :src="HoverImage(image.imageHover)" alt="" > -->
+        </div>
     </main>
 </template>
 
 <style lang="scss" scoped>
-@use '../styles/general.scss'
+@use '../styles/general.scss' as *;
+
+main{
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    
+    div{
+        margin: 20px auto;
+        width: calc(100% / 3);
+        text-align: center;
+
+        img{
+            width: 200px;
+        }
+    }
+}
 </style>
